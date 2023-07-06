a850392dcbf6938437db76106f623dccf180abd9
omar abdrbuo <omarkh3010@gmail.com>
2023-07-06 19:58:28 +0300

    sevice problem

--- db/migrate/20230706165546_add_services_to_planners.rb
+++ db/migrate/20230706165546_add_services_to_planners.rb
@@ -1,5 +1,5 @@
 class AddServicesToPlanners < ActiveRecord::Migration[7.0]
   def change
-    add_column :planners, :services, :text, array: true, default: []
+    add_column :planners, :services, :string
   end
 end
